bbeb7648c71e7f43b2abdb5d1ae7b3d56b3d6a83
JoaoLCordeiro <joaolucascordeiro@gmail.com>
2022-08-19 17:52:01 -0300

    comecei algo da 11 mas n sei...

---
+++ Semana11/Bares.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int TAM_VET = 21234;
+
+vector<int> segmentos (TAM_VET, 0);
+
+int valor_abs (int a){
+	return (a + 10000);
+}
+
+int main() {
+	int n_bares, n_op;
+	cin >> n_bares >> n_op;
+
+	for (){
+		
+	}
+}
